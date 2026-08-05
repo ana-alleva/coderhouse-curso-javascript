4a341ac6c5a5e34e29b5a2722d7150d252e514d9
Ana Clara Alleva <aclaraalleva@gmail.com>
2026-08-05 19:18:41 -0300

    entregable 1

---
+++ js/main.js
@@ -0,0 +1,13 @@
+console.log("Prueba");
+
+const nombre = prompt("Cual es tu nombre?");
+const anioDeNacimiento = prompt("En que anio naciste?");
+const mascota = prompt("Como se llama tu mascota?");
+console.log(
+  "El nombre del usuario es " +
+    nombre +
+    " y nacio en " +
+    anioDeNacimiento +
+    " su mascota se llama " +
+    mascota,
+);
