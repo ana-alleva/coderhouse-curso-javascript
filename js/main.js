ebfce958622032d06dda312014d64ace63d53722
Ana Clara Alleva <aclaraalleva@gmail.com>
2026-08-30 13:26:50 -0300

    Ejercicio Preentrega 3

--- js/main.js
+++ js/main.js
@@ -1,17 +1,58 @@
-/* Ejercicio Simulador de Calificaciones */
+/* Ejercicio Simulador de Compra */
 
-const cantidadNotas = Number(prompt("¿Cuántas notas querés cargar?"));
+const Tomate = 40;
+const JabonLiquido = 30;
+const Perfume = 69;
 
-for (let i = 1; i <= cantidadNotas; i++) {
-  const nota = Number(prompt(`Ingresá la nota número ${i}`));
+function calcularProducto(nombre, cantidad) {
+  let subtotal;
 
-  if (nota >= 8) {
-    alert("Muy buen resultado");
-  } else if (nota >= 6) {
-    alert("Aprobado");
+  if (nombre === "Tomate") {
+    subtotal = Tomate * cantidad;
+  } else if (nombre === "JabonLiquido") {
+    subtotal = JabonLiquido * cantidad;
+  } else if (nombre === "Perfume") {
+    subtotal = Perfume * cantidad;
   } else {
-    alert("Desaprobado");
+    alert("No tenemos ese producto");
+    subtotal = 0;
   }
+
+  return subtotal;
 }
 
-console.log("Todas las notas fueron evaluadas");
+function calcularDescuento(total) {
+  let precioFinal = total;
+
+  if (total >= 50000) {
+    const descuento = (total * 10) / 100;
+    precioFinal = total - descuento;
+  }
+
+  return precioFinal;
+}
+
+function mostrarResultado(resultado) {
+  alert("El total de la compra es: $" + resultado);
+}
+
+let totalCompra = 0;
+let continuar = "si";
+
+while (continuar === "si") {
+  const producto = prompt(
+    "Qué producto vas a comprar? Tomate, JabonLiquido o Perfume",
+  );
+
+  const cantidad = Number(prompt("Cuántos " + producto + " vas a comprar?"));
+
+  const subtotal = calcularProducto(producto, cantidad);
+
+  totalCompra = totalCompra + subtotal;
+
+  continuar = prompt("Querés agregar otro producto? si o no");
+}
+
+const totalConDescuento = calcularDescuento(totalCompra);
+
+mostrarResultado(totalConDescuento);
